492e82fe0f1cac58a3b3f341825580b69885f807
mlausberg <mlausberg@client15.ledscomgmbh.local>
2014-12-08 12:44:54 +0100

    Merge origin/master
    Conflicts:
    	src/factories/issueFactory.hh

--- test/testTaskFactory.hh
+++ test/testTaskFactory.hh
@@ -23,7 +23,7 @@ class TestTaskFactory extends TestCase {
         }
 
 	public function testCreateAll() {
-            $vec = $this->fac->createAll();
+            $vec = $this->fac->getAll();
             var_dump($vec);
             $this->t = $vec[0];
             
