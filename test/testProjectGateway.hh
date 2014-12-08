210433aee3415633a87235a110f1831854f70367
mlausberg <mlausberg@client15.ledscomgmbh.local>
2014-12-08 13:22:58 +0100

    Merge origin/master
    Conflicts:
    	src/factories/issueFactory.hh

--- test/testProjectGateway.hh
+++ test/testProjectGateway.hh
@@ -25,6 +25,6 @@ class TestProjectGateway extends TestCase {
 	public function testGetProjectsByUser() {
             $this->user->setID(1);
             $vec = $this->gate->getByUser($this->user);
-            assertEqualsString($vec[1],"Hack IDE");
+            $this->assertEqualsString($vec[1],"Hack IDE");
         }
 }
